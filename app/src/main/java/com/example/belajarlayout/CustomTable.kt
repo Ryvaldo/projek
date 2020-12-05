5367b7a0c94112afdab3ac720d92e8f143dfab80
Ryvaldo <void.san.17@gmail.com>
2020-12-05 21:02:40 +0700

    modul4

---
+++ app/src/main/java/com/example/belajarlayout/CustomTable.kt
@@ -0,0 +1,20 @@
+package com.example.belajarlayout
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_layout_table.*
+
+class CustomTable : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_table)
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
+    }
+}
+
+
---
+++ app/src/main/java/com/example/belajarlayout/LinearLayout.kt
@@ -0,0 +1,12 @@
+package com.example.belajarlayout
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LinearLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout)
+    }
+}
